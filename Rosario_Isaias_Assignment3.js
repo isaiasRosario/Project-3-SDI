724778ee2afc8e6fbdd69825b0fc2a8159a05cc4
r16902 <irosario@fullsail.edu>
2013-08-22 22:01:04 -0400

    Added Array and Object Function
    Added array and Object Function with return values.

--- Rosario_Isaias_Assignment3.js
+++ Rosario_Isaias_Assignment3.js
@@ -9,7 +9,8 @@
 
 // Global Variables
 var monster = "Big Blue Monster",
-	monsterHealth = 100
+	monsterHealth = 100,
+	hideArr = ["Car", "Bus", "Truck"]
 ;
 	
 
@@ -65,22 +66,54 @@ var shooterGunUsed = function(shooter1, shooter2){
 
 // Boolean Function
 var monsterChase = function(tof){
-	console.log("Is it really " + tof + " that a " + monster + " is chasing us!");
+	var chaseTxt =  " is chasing us!"
+	console.log("Is it really " + tof + " that a " + monster + chaseTxt);
 	
 	return tof;
 };
 
+// Array Function
+var hideOptions = function (arrArg){
+	var arr = arrArg;
+	for(i = 0; i < 2; i++){
+		console.log("We can hide behind the " + arr[i] + " or, ");
+		for(ii = 0; ii < 1; ii++){
+			console.log("Is it really safe to take cover here?");
+		};
+	};
+	console.log("We can hide behind the " + arr[2] + ".");
+	console.log("Ok this " + arr[2] + " looks like a safe cover spot.")
+	
+	return arr;
+};
+
+// Object Function
+var gunChoices = function (obj){
+	console.log("We have 3 guns to choose from " + obj.pistol + ", " +
+				obj.riffle + ", and the " + obj.shotgun + ".");
+				
+	return obj;
+};
+
 // Main Code
 
-var isMonsterChase = monsterChase(true);
+var isMonsterChase = monsterChase(true); // returns boolean value
  
 console.log("Ahh its " + isMonsterChase + " there is a " + monster +
 			" out here lets find a gun to protect ourselves!");
 
+var allGuns = gunChoices(shootGun.selectGun) // return object value
+
 var shooterName = shooterGunUsed(shooterNames.shooters[0].shooterName,
-				shooterNames.shooters[1].shooterName); // returns string
+				shooterNames.shooters[1].shooterName); // returns string value
 
 isSafetyOn(shootGun.safety);
+
+console.log("Where can we hide?");
+
+var weHide = hideOptions(hideArr); // returns array value
+
+console.log
 
 console.log(shooterName + " noticed that the " + shootGun.selectGun.pistol +
 			" has a total power boost of " + shootGun.gunPower() + ".");
